1f8bd87901c79118262550d914eb068604fcb9dd
Joshua <101841420+JoshuaSL@users.noreply.github.com>
2022-09-16 19:20:06 +0200

    Fix drugs patch

--- resources/sCore/modules/Illegal/Drugs/sellnpc_cl.lua
+++ resources/sCore/modules/Illegal/Drugs/sellnpc_cl.lua
@@ -1,154 +1,50 @@
 ESX = nil 
-local drugSell, blip = false, false, 
 
-Citizen.CreateThread(function()
+CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('Sneakyesx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
+		Wait(0)
 	end
 end) 
 
-Neo = {
-    distanceAnelysingStart = {20, 40},
-    cooldown = {5000, 10000},  
-    callCops = {1, 5},
-    drugs = { 
-        ["weed_pooch"] = {index = 1, prices = {50, 65}}, 
-        ["meth_pooch"] = {index = 2, prices = {115, 130}},
-        ["coke_pooch"] = {index = 3, prices = {135, 145}}
-    },
-    indexes = {
-        [1] = "weed_pooch",
-        [2] = "meth_pooch",
-        [3] = "coke_pooch"
-    }
-}  
+Drugs = {}
 
-RegisterNetEvent('neo:askToStart')
-AddEventHandler('neo:askToStart', function(data)
-    local clicked = false
-    local tbl = {}
-    for k, v in pairs(data) do 
-        if v.weed_pooch then 
-            table.insert(tbl, Neo.drugs["weed_pooch"].index)
-        elseif v.coke_pooch then 
-            table.insert(tbl, Neo.drugs["coke_pooch"].index)
-        elseif v.meth_pooch then 
-            table.insert(tbl, Neo.drugs["meth_pooch"].index)
-        end 
+Drugs.Items = {
+    ["weed_pooch"] = true,
+    ["meth_pooch"] = true,
+    ["coke_pooch"] = true
+}
+
+Drugs.Sell = false
+
+function Drugs:GetRandomCoords()
+    if Drugs.Sell == nil or Drugs.Sell == false then return end
+    
+    local playerCoords = GetEntityCoords(PlayerPedId())
+    local CoordsDrugs, SafeCoords = GetSafeCoordForPed(playerCoords.x + GetRandomIntInRange(-40, 40), playerCoords.y + GetRandomIntInRange(-40, 40), playerCoords.z, true, 0, 16)
+
+    if not CoordsDrugs or GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, SafeCoords.x, SafeCoords.y, SafeCoords.z) < 20 then
+        return
     end
 
-    startSell(Neo.indexes[tbl[math.random(#tbl)]])
-
-end) 
- 
-RegisterNetEvent('neo:ActivDrugsSell')
-AddEventHandler('neo:ActivDrugsSell', function(bool)
-    if not drugSell then  
-        local cooldown = math.random(Neo.cooldown[1], Neo.cooldown[2])
-        DrawSub("Vous êtes a la recherche de ~b~clients~s~.", cooldown)
-        Wait(cooldown) 
-        TriggerServerEvent('neo:trytoStart')
-    else
-        if blip then 
-            RemoveBlip(blip) 
-        end 
-        randomPos = false
-        boolTaken = false
-        ShowAboveRadarMessage("~r~Vous venez d'arrêter de chercher des clients.")
-    end
-    drugSell = bool
-end)
-
-RegisterNetEvent('neo:restartFarming')
-AddEventHandler('neo:restartFarming', function()
-    local cooldown = math.random(Neo.cooldown[1], Neo.cooldown[2])
-    DrawSub("Vous êtes a la recherche de ~b~clients~s~.", cooldown)
-    Wait(cooldown)
-    TriggerServerEvent('neo:trytoStart')
-end) 
-
-function startSell(item)
-    ESX.TriggerServerCallback('neo:checkCops', function(cb)
-        if cb then 
-            local pPos = GetEntityCoords(PlayerPedId())
-            local randomPos = getRandomPos()
-            while randomPos == nil or randomPos == false do 
-                randomPos = getRandomPos()
-            end
-            if randomPos then
-                local boolTaken = false
-                createBlip(randomPos, {501, 3, "Point de vente", 0.7})
-            
-                Citizen.CreateThread( function()
-                    while not boolTaken and drugSell do
-                        msec = 0
-                        local pPos = GetEntityCoords(PlayerPedId())
-                        local dist = #(vec2(pPos.x, pPos.y) - randomPos)
-                        if not IsPedInAnyVehicle(PlayerPedId(), true) then 
-                            DrawSub("Un point de livraison a été marqué dans la zone (~b~"..math.floor(dist).."m~s~)", 1)
-                            if dist < 10 then
-                                DrawMarker(1, randomPos.x, randomPos.y, pPos.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0, 170, 255, 180, false, false, 2, false, false, false, false)
-                            end
-                            if dist < 7 then  
-                                msec = 1
-                                ESX.ShowHelpNotification("Appuyez sur ~INPUT_PICKUP~ pour effectuer la ~b~Vente")
-                                if IsControlJustPressed(0, 51) then 
-                                    boolTaken = true 
-                                    RemoveBlip(blip) 
-                                    startAnimNeo("random@domestic", "pickup_low")
-                                    TriggerServerEvent('neo:sellDrugs', item, math.random(Neo.drugs[item].prices[1], Neo.drugs[item].prices[2]), Neo.drugs)
-                                    local randomCallCops = math.random(Neo.callCops[1], Neo.callCops[2])
-                                    if randomCallCops == 1 then   
-                                        TriggerServerEvent("sCall:SendCallMsg", "Vente de drogue en cours", GetEntityCoords(PlayerPedId()), "police", false)
-                                        TriggerServerEvent("sCall:SendCallMsg", "Vente de drogue en cours", GetEntityCoords(PlayerPedId()), "lssd", false)
-                                    end
-                                end
-                            end 
-                        end
-                        Citizen.Wait(msec) 
-                    end
-                end)
-            end
-        else
-            ShowAboveRadarMessage("Il n’y a aucun clients intéressé. (Pas assez de flic en service)")
-        end
-    end)
-end
-
-function getRandomPos()
-    while true and drugSell do 
-        Wait(0)
-        local pPos = GetEntityCoords(PlayerPedId())
-        local CoordsDrugs, SafeCoords = GetSafeCoordForPed(pPos.x + GetRandomIntInRange(-40, 40), pPos.y + GetRandomIntInRange(-40, 40), pPos.z, true, 0, 16)
-        if not CoordsDrugs then
-            return
-        end
-        if GetDistanceBetweenCoords(pPos.x, pPos.y, pPos.z, SafeCoords.x, SafeCoords.y, SafeCoords.z) < 20 then
-            return
-        end
-        if IsPedInAnyVehicle(PlayerPedId(), true) then
-            return
-        end
-        DrawSub("Vous êtes a la recherche de ~b~clients~s~.", 1)
-        return vec2(SafeCoords.x, SafeCoords.y)
-    end
+    return vector3(SafeCoords.x, SafeCoords.y, SafeCoords.z - 1.0)
 end 
 
-function DrawSub(msg, time)
-	ClearPrints()
-	BeginTextCommandPrint('STRING')
-	AddTextComponentSubstringPlayerName(msg)
-	EndTextCommandPrint(time, 1)
+function Drugs:PlayerHasItem()
+    local hasItem = false
+
+    for _,item in pairs(ESX.PlayerData.inventory) do
+        if item.count > 0 then
+            if Drugs.Items[item.name] then
+                hasItem = item.name
+            end
+        end
+    end
+
+    return hasItem
 end
 
-function startAnimNeo(lib, anim)
-	ESX.Streaming.RequestAnimDict(lib, function()
-		TaskPlayAnim(PlayerPedId(), lib, anim, 8.0, -8.0, -1, 0, 0.0, false, false, false)
-	end)
-end
-
-function createBlip(pos, data)
+function Drugs:CreateBlip(pos, data)
     if blip then 
         RemoveBlip(blip) 
     end 
@@ -160,16 +56,110 @@ function createBlip(pos, data)
     BeginTextCommandSetBlipName('STRING')
 	AddTextComponentString(data[3])
 	EndTextCommandSetBlipName(blip)
-end   
+end
 
-RegisterCommand('drugs', function()
-    if drugSell then 
-        startSell()
-    else
-        ESX.ShowNotification("Vous avez ~r~arrêter~s~ de vendre.")
+function Drugs:DrawText(msg, time)
+	ClearPrints()
+	BeginTextCommandPrint('STRING')
+	AddTextComponentSubstringPlayerName(msg)
+	EndTextCommandPrint(time, 1)
+end
+
+function Drugs:Anim(lib, anim)
+	ESX.Streaming.RequestAnimDict(lib, function()
+		TaskPlayAnim(PlayerPedId(), lib, anim, 8.0, -8.0, -1, 0, 0.0, false, false, false)
+	end)
+end
+
+function Drugs:StartBoucleForSelling()
+    local playerHasItem = Drugs:PlayerHasItem()
+    if playerHasItem == nil or playerHasItem == false then return ESX.ShowNotification("~r~Vous n'avez pas ce qu'il faut sur vous.") end
+
+    Drugs.Sell = not Drugs.Sell
+
+    local delivery, deliveryPos = nil, nil
+    while true do
+        if Drugs.Sell == nil or Drugs.Sell == false then
+            if delivery then
+                if DoesBlipExist(delivery.blip) then
+                    RemoveBlip(delivery.blip)
+                    delivery.blip = nil
+                end
+                delivery = nil
+            end
+            break
+        end
+
+        playerHasItem = Drugs:PlayerHasItem()
+        if playerHasItem == nil or playerHasItem == false then 
+            if delivery then
+                if DoesBlipExist(delivery.blip) then
+                    RemoveBlip(delivery.blip)
+                    delivery.blip = nil
+                end
+                playerHasItem = nil
+                delivery = nil
+            end
+            ESX.ShowNotification("~r~Vous n'avez plus assez de marchandise.")
+            break
+        end
+
+        local Interval = 250
+
+        while deliveryPos == nil do
+            Wait(0)
+            Drugs:DrawText("Vous êtes a la recherche de ~b~clients~s~.", 1)
+            deliveryPos = Drugs:GetRandomCoords()
+        end
+        
+        if deliveryPos ~= nil and delivery == nil then
+            delivery = {
+                point = deliveryPos, 
+                blip = Drugs:CreateBlip(deliveryPos, {501, 3, "Livraison", 0.7}),
+                entity = nil
+            }
+            
+            while delivery.point == nil do
+                Wait(50)
+            end
+
+            deliveryPos = nil
+        else
+            Interval = 0
+            Drugs:DrawText("Un point de livraison a été marqué dans la ~b~zone~s~.", 1)
+            if #(GetEntityCoords(PlayerPedId())-delivery["point"]) < 3.0 then
+                ESX.ShowHelpNotification("Appuyez sur ~INPUT_CONTEXT~ pour ~g~déposer~s~ votre ~b~livraison~s~.")
+                if IsControlJustReleased(0, 54) then
+                    Drugs:Anim("random@domestic", "pickup_low")
+                    FreezeEntityPosition(PlayerPedId(), true)
+                    Wait(GetAnimDuration("pickup_object", "pickup_low") * 400)
+                    FreezeEntityPosition(PlayerPedId(), false)
+                    if DoesBlipExist(delivery.blip) then
+                        RemoveBlip(delivery.blip)
+                        delivery.blip = nil
+                    end
+                    local random = math.random(1, 10)
+                    if random == 3 then
+                        TriggerServerEvent("sCall:SendCallMsg", "Vente de drogue en cours", GetEntityCoords(PlayerPedId()), "police", false)
+                        TriggerServerEvent("sCall:SendCallMsg", "Vente de drogue en cours", GetEntityCoords(PlayerPedId()), "lssd", false)
+                    end
+                    TriggerServerEvent("Drugs:Sell", playerHasItem)
+                    delivery = nil
+                end
+            elseif #(GetEntityCoords(PlayerPedId())-delivery["point"]) < 30.0 then
+                DrawMarker(1,delivery["point"],0.0,0.0,0.0,0.0,0.0,0.0,1.0,1.0,1.0,162,255,162,80,0,0,0,0,0,0,0)
+            end
+        end
+
+        Wait(Interval)
     end
+end
+
+RegisterCommand("drugs", function()
+    Drugs:StartBoucleForSelling()
 end)
 
-
-
-
+RegisterNetEvent("Drugs:SellDrugs")
+AddEventHandler("Drugs:SellDrugs", function()
+    Drugs:StartBoucleForSelling()
+end)
--- resources/sCore/modules/Illegal/Drugs/sellnpc_sv.lua
+++ resources/sCore/modules/Illegal/Drugs/sellnpc_sv.lua
@@ -8,52 +8,27 @@ local drugsList = {
     ["meth_pooch"] = true
 }
 
-RegisterNetEvent("neo:trytoStart")
-AddEventHandler("neo:trytoStart", function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local canSell, tbl = false, {}
-    for k, v in pairs(drugsList) do  
-        if xPlayer.getInventoryItem(k).count > 0  then
-            canSell = true
-            table.insert(tbl, {[k] = xPlayer.getInventoryItem(k).count})
-        end 
-    end   
-    if canSell then
-        TriggerClientEvent('neo:askToStart', xPlayer.source, tbl)
-    else
-        TriggerClientEvent('Sneakyesx:showNotification', xPlayer.source, "~r~Vous n'avez pas de drogue sur vous.")
-        TriggerClientEvent("SneakyLife:StopSellDrugs", source)
-    end 
-end) 
+RegisterServerEvent("Drugs:Sell", function(itemName)
+    local playerSrc = source
+    if (not playerSrc) then return end
 
-RegisterNetEvent("neo:sellDrugs")
-AddEventHandler("neo:sellDrugs", function(item, price, Neo)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    
-    if Neo[item].prices[2] >= price and Neo[item].prices[1] <= price and Neo ~= nil then 
-        TriggerClientEvent('neo:restartFarming', xPlayer.source)
-        TriggerClientEvent("Sneakyesx:showNotification", source, "Vous avez vendu ~b~<C>"..ESX.GetItemLabel(item).."~s~</C> pour ~g~<C>"..price.."$</C>~s~.")
-        xPlayer.removeInventoryItem(item, 1)
-        xPlayer.addAccountMoney('cash', price) 
-    else 
-        DropPlayer(source, "Tentative de give")
-    end  
-end)
-  
+    local playerSelected = ESX.GetPlayerFromId(playerSrc)
+    if (not playerSelected) then return end
 
+    if (not drugsList[itemName]) then return end
 
-ESX.RegisterServerCallback('neo:checkCops', function(source, cb)
-    local cops = 0
-    local xPlayers = ESX.GetPlayers()
-    for i = 1, #xPlayers do
-        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-        if xPlayer.job.name == 'police' or xPlayer.job.name == 'lssd' then
-            cops = cops + 1
+    if (playerSelected.getInventoryItem(itemName).count < 1) then return end
+
+    if playerSelected.getInventoryItem(itemName).count >= 1 then 
+        if itemName == "weed_pooch" then
+            random = math.random(55, 90)
+        elseif itemName == "meth_pooch" then
+            random = math.random(90, 110)
+        elseif itemName == "coke_pooch" then
+            random = math.random(90, 110)
         end
-    end 
-    if cops >= 3 then
-        cb(true) 
-    else
-        cb(false)
+        playerSelected.removeInventoryItem(itemName, 1)
+        playerSelected.addAccountMoney("cash", random)
+        playerSelected.showNotification("Vous avez vendu votre ~b~"..ESX.GetItemLabel(itemName).."~s~ pour "..random.."~g~$~s~.")
     end
 end)
